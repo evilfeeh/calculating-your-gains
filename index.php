20e5798e1460143b575c291650e31db27f8cf05d
Felipe Gonçalves dos Santos <felipegoncalvessantos@hotmail.com>
2017-03-08 13:02:50 -0300

    first commit
    (html code)

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Calculo Basal</title>
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+	<h1>Seja bem-vindo a calculadora de indice basal em PHP</h1>
+	<aside id="barra-direita">
+		<h2>alimentos menos caloricos:</h2>
+		[imagens]
+		subs
+		[imagens]
+		subs
+		[imagens]
+		subs
+	</aside>
+
+	<div id="calculo">
+	<h2>calculadora Basal</h2>
+	<p>digite seu peso:</p>
+	<p>digite sua idade:</p>
+	<p>digite sua altura em CM:</p>
+	<p>digite o seu grau de atividade fisica:</p>
+	</div>
+
+	<div id="resultado">
+	<h2>resultado:</h2>
+	<p>quantidade de caloria para emagrecer:</p>
+	<p>quantidade de caloria para manter o peso:</p>
+	<p>quantidade de caloria para engordar:</p>
+	</div>
+
+	<aside id="barra-esquerda">
+	<h2>alimentos mais caloricos:</h2>
+	[imagens]
+	subs
+	[imagens]
+	subs
+	[imagens]
+	subs
+	</aside>
+
+	<footer id="rodape">
+	<p>Este site foi construido por Felipe Gonçalves dos Santos</p>
+	</footer>
+</body>
+</html>
