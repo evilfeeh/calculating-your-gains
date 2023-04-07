fe784bbace243f6387bb91ec1620771855f6ac13
felipe goncalves <felipe.goncalves@semantix.ai>
2023-04-07 13:49:29 -0300

    feat: add multiplicator object

---
+++ src/app/utils/multiplicators.ts
@@ -0,0 +1,16 @@
+const multiplicator = {
+  "female": {
+    "weight": 9.6,
+    "height": 1.8,
+    "age": 4.7,
+    "factor": 65
+  },
+  "male": {
+    "weight": 13.7,
+    "height": 5.0,
+    "age": 6.8,
+    "factor": 66
+  }
+}
+
+export default multiplicator
