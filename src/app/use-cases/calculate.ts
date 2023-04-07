ff4bb11d67384b26e0642b63864df4922dc28df5
felipe goncalves <felipe.goncalves@semantix.ai>
2023-04-07 14:32:26 -0300

    feat: increase calculate class

--- src/app/use-cases/calculate.ts
+++ src/app/use-cases/calculate.ts
@@ -1,29 +1,50 @@
-import calculateClass from "../interfaces/calculateClass"
-import personData from "../interfaces/personData"
+import phisicalActivityLevel from "../utils/phisical-activity-level"
+import calculateClass from "../interfaces/calculate-class"
+import personData from "../interfaces/person-data"
 import multiplicator from "../utils/multiplicators"
 
-class Calculate implements calculateClass {
-  data: personData
+export default class Calculate implements calculateClass {
+  person: personData
   weightMultiplicator: number
   heightMultiplicator: number;
   ageMultiplicator: number;
   kaloriesBase: 500; 
   factor: number;
+  activityLevel: number;
+  score: {
+    weight: number;
+    height: number;
+    age: number;
+  }
 
   constructor(person: personData) {
-      this.weightMultiplicator = multiplicator[person.sex].weight
-      this.heightMultiplicator = multiplicator[person.sex].height
-      this.ageMultiplicator = multiplicator[person.sex].age
-      this.factor = multiplicator[person.sex].factor
+    this.person = person
+    this.activityLevel = person.phisicalAcitivityLevel
+    this.weightMultiplicator = multiplicator[person.sex].weight
+    this.heightMultiplicator = multiplicator[person.sex].height
+    this.ageMultiplicator = multiplicator[person.sex].age
+    this.factor = multiplicator[person.sex].factor
+    this._calculateScore()
   }
 
-  keep () {
-    
+  toKeep () {
+    let scoreWithFactor = this.factor + this.score.weight + this.score.height + this.score.age
+    return phisicalActivityLevel[this.activityLevel] + scoreWithFactor
   }
-  lose () {
 
+  toLose () {
+    return this.toKeep() - this.kaloriesBase
   }
-  gain () {
 
+  toGain () {
+    return this.toKeep() + this.kaloriesBase
+  }
+
+  _calculateScore () {
+    this.score = {
+      weight: this.person.weight * this.weightMultiplicator,
+      height: this.person.height * this.heightMultiplicator,
+      age: this.person.age * this.ageMultiplicator
+    }
   }
 }
