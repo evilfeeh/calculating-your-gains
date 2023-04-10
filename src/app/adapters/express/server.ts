bd950e29af025e465017986e6a115bc1c28fbb35
felipe goncalves <felipe.goncalves@semantix.ai>
2023-04-10 15:34:18 -0300

    feat: create simple server

---
+++ src/app/adapters/express/server.ts
@@ -0,0 +1,24 @@
+import express from 'express'
+import Calculate from '../../domain/use-cases/calculate'
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded())
+
+app.post('/calculate', (req, res) => {
+  const { personData } = req.body
+  const calculate = new Calculate(personData)
+
+  const result = {
+    'keep': calculate.toKeep(),
+    'gain': calculate.toGain(),
+    'lose': calculate.toLose()
+  }
+
+  res.status(200).send(result)
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`listen http server on port: ${process.env.PORT}`)
+})
