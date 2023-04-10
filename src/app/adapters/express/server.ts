2a9883da2f0269b984d3daa5f84b81110cf30de8
felipe goncalves <felipe.goncalves@semantix.ai>
2023-04-10 18:33:02 -0300

    fix: add parameter extended

--- src/app/adapters/express/server.ts
+++ src/app/adapters/express/server.ts
@@ -4,7 +4,7 @@ import Calculate from '../../domain/use-cases/calculate'
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded())
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
 
