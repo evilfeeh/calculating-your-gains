dfe3fcc66f1eb75bca74487021733bca95a838ac
felipegsantos15 <felipegoncalvesdsantos@gmail.com>
2023-05-10 21:35:45 -0300

    feat: implements modal results

--- public/index.js
+++ public/index.js
@@ -8,6 +8,7 @@ createApp({
       age: '',
       female: '',
       male: '',
+      sex: '',
       phisicalAcitivityLevel: '',
       toLoseCalories: '',
       toKeepCalories: '',
@@ -15,8 +16,22 @@ createApp({
     }
   },
   methods: {
-    calculateWeight () {
-      console.log(this)
+    async calculateWeight () {
+      // const result = await this.$http.post(
+      //   "http://localhost:3000/weight",
+      //   {
+      //     height: this.height,
+      //     weight: this.weight,
+      //     male: this.male,
+      //     sex: this.sex,
+      //     phisicalAcitivityLevel: this.phisicalAcitivityLevel,
+      //   }
+      // );
+
+      const modal = new bootstrap.Modal(document.getElementById('resultado'), {
+        keyboard: true
+      })
+      modal.show()
     }
   }
 }).mount('#appWeight')
