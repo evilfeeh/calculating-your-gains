c52893570272a54f246abf7f431a2b7ec89d8dff
felipegsantos15 <felipegoncalvesdsantos@gmail.com>
2023-05-17 21:48:15 -0300

    refactor: separe request function

--- public/index.js
+++ public/index.js
@@ -17,20 +17,32 @@ createApp({
   },
   methods: {
     async calculateWeight () {
-      const request = await fetch("http://localhost:3000/weight", {
-        method: "POST",
-        body: {
-          height: this.height,
-          weight: this.weight,
-          sex: this.sex,
-          phisicalAcitivityLevel: this.phisicalAcitivityLevel,
-        },
-      });
+      const result = await this.makeRequest()
+
+      this.toLoseCalories = result.lose
+      this.toKeepCalories = result.keep
+      this.toGainCalories = result.gain
 
       const modal = new bootstrap.Modal(document.getElementById('resultado'), {
         keyboard: true
       })
       modal.show()
+    },
+    async makeRequest() {
+      const result = await fetch("http://localhost:3000/weight", {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({
+          height: this.height,
+          weight: this.weight,
+          sex: this.sex,
+          age: this.age,
+          phisicalAcitivityLevel: this.phisicalAcitivityLevel,
+        }),
+      });
+      return result.json()
     }
   }
 }).mount('#appWeight')
