cca29d9d854bb6be4e7963ed29e409017ad166d5
felipegsantos15 <felipegoncalvesdsantos@gmail.com>
2023-05-23 21:46:43 -0300

    feat: adding dark mode

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,7 @@
-import './globals.css'
+"use client";
 import { Inter } from 'next/font/google'
+import { ColorModeScript } from '@chakra-ui/react'
+import theme from './theme';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className} >{children}
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      </body>
     </html>
   )
 }
