c5ed0896fa278584a2f4349d340165d0ced91c58
felipegsantos15 <felipegoncalvesdsantos@gmail.com>
2023-05-26 21:12:28 -0300

    refactor: frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { Center, ChakraProvider, Container, Grid, GridItem, Text } from '@chakra-ui/react';
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
+import { Center, ChakraProvider, Grid, GridItem, Text } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import Weight from './components/weight';
 import Image from 'next/image'
 import logo from '../../public/imagens/emojii.svg'
@@ -8,32 +8,25 @@ import logo from '../../public/imagens/emojii.svg'
 export default function Home() {
   return (
     <ChakraProvider>
-      
         <Grid templateAreas={`'header'`} gap={5} templateColumns='repeat(2, 1fr)'>
           <GridItem area={'header'} colSpan={4} rowSpan={1} bg={'green'}>
-            <Image src={logo} width={50} height={50} alt="double biceps" />
-            <Text >Calculating your gains</Text>
+            <Image src={logo} width={100} height={100} alt="double biceps" />
+            <Text>Calculating your gains</Text>
           </GridItem>
-          <GridItem h={700} colSpan={2} colEnd={2} bg={'red'}>
-            <Center>
-              esquerda
-            </Center>
+          <GridItem h={700} colSpan={2} colEnd={2}>
+            <Box h='500px' p={'5'} mt={'5'} >
+              <Center>
+                <Image src={logo} width={300} height={300} alt="double biceps"/>
+              </Center>
+              <Center>
+                <Text textStyle={'bold'} fontSize={'5xl'} as='b'>CALCULATING YOUR GAINS</Text>
+              </Center>
+            </Box>
           </GridItem>
           <GridItem h={700} colSpan={2} colStart={2} colEnd={4}>
-          <Tabs>
-            <TabList>
-              <Tab>Weight Calories Consume</Tab>
-              <Tab>Water Daily Consume</Tab>
-            </TabList>
-            <TabPanels>
-              <TabPanel>
-              <Weight />
-              </TabPanel>
-              <TabPanel>
-                <p>two!</p>
-              </TabPanel>
-            </TabPanels>
-          </Tabs>
+            <Box  h='500px' p={'5'} mt={'5'}>
+                <Weight />
+            </Box>
           </GridItem>
           <GridItem colSpan={4} bg={'purple'} rowSpan={1}>footer</GridItem>
         </Grid>
