743e0794d15501a2ae6dc7285190ae03061d06ac
felipegsantos15 <felipegoncalvesdsantos@gmail.com>
2023-06-01 19:39:55 -0300

    refactor: simplify component

--- frontend/src/app/components/weight.tsx
+++ frontend/src/app/components/weight.tsx
@@ -10,46 +10,28 @@ export default function Weight() {
     <ChakraProvider>
       <Text fontSize={'4xl'} textAlign={'center'} >Are You Eating Enough?</Text>
       <Text fontSize={'sm'} textAlign={'center'}>This Calc will help you to figure out</Text>
-      <Box mt={'5'}>
-        <Center>
-          <FormControl>
-            <FormLabel fontSize={'1xl'}>What is your birth gender?</FormLabel>
-            <RadioGroup onChange={setValue} value={value}>
-              <Stack direction='row'>
-                <Radio value='1'>Female</Radio>
-                <Radio value='2'>Male</Radio>
-              </Stack>
-            </RadioGroup>
-          </FormControl>
-        </Center>
-        <Center mt={'4'}>
-          <FormControl>
-            <Input padding={2} borderRadius={4} border='1px' borderColor={'#276749'} placeholder="How`s your weight" size='md' id="weight" />
-          </FormControl>
-        </Center>
-        <Center mt={'4'}>
-          <FormControl>
-            <Input padding={2} borderRadius={4} border='1px' borderColor={'#276749'} placeholder="How's your height in CM" size='md' id="height" />
-          </FormControl>
-        </Center>
-        <Center mt={'4'}>
-          <FormControl>
-            <Input padding={2} borderRadius={4} border='1px' borderColor={'#276749'} placeholder="How old are you" size='md' id='age'></Input>
-          </FormControl>
-        </Center>
-        <Center mt={'4'}>
-          <FormControl>
-            <Select padding={2} borderRadius={4} border='1px' borderColor={'#276749'}size='md'>
-              <option value="200">Sedentário</option>
-              <option value="600">Atividade leve - 3x semana</option>
-              <option value="1000">Atividade moderada - 5x semana</option>
-              <option value="1200">Atividade pesada - 6x seman</option>
-            </Select>
-          </FormControl>
-        </Center>
-        <Center mt={'4'}>
-          <Button colorScheme='teal' variant='outline' size='md'> Calculate</Button>
-        </Center>
+      <Box>
+        <FormControl>
+          <FormLabel fontSize={'1xl'}>What is your birth gender?</FormLabel>
+          <RadioGroup onChange={setValue} value={value}>
+            <Stack direction='row'>
+              <Radio value='1'>Female</Radio>
+              <Radio value='2'>Male</Radio>
+            </Stack>
+          </RadioGroup>
+          <Input borderRadius={4} border='1px' mt={'5'} borderColor={'#276749'} placeholder="How`s your weight" size='md' id="weight" />
+          <Input borderRadius={4} border='1px' mt={'5'} borderColor={'#276749'} placeholder="How's your height in CM" size='md' id="height" />
+          <Input borderRadius={4} border='1px' mt={'5'} borderColor={'#276749'} placeholder="How old are you" size='md' id='age'></Input>
+          <Select borderRadius={4} border='1px' mt={'5'} borderColor={'#276749'} size='md'>
+            <option value="200">Sedentário</option>
+            <option value="600">Atividade leve - 3x semana</option>
+            <option value="1000">Atividade moderada - 5x semana</option>
+            <option value="1200">Atividade pesada - 6x seman</option>
+          </Select>
+          <Center>
+            <Button colorScheme='teal' mt={'5'} variant='outline' size='md'> Calculate</Button>
+          </Center>
+        </FormControl>
       </Box>
     </ChakraProvider>
   )
