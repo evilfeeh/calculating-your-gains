bc86562dbd5cda51db2cd8c4d82e8e240d3ce09d
felipegsantos15 <felipegoncalvesdsantos@gmail.com>
2023-06-21 21:53:09 -0300

    feat: show results on a modal

---
+++ frontend/src/app/components/alert.tsx
@@ -0,0 +1,33 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  Text
+} from '@chakra-ui/react'
+export default function AlertResults({ results, isVisible, onClose }: any ) {
+  console.log(results.keep, results.lose, results.lose)
+  return (
+    <Modal isOpen={isVisible} onClose={onClose}>
+    <ModalOverlay />
+    <ModalContent>
+      <ModalHeader>Results</ModalHeader>
+      <ModalCloseButton />
+      <ModalBody>
+        To <b>lose</b> weight: <br/> Consume <Text as='mark'>{results.lose}</Text> calories per day <br/>
+        To <b>keep</b> weight: <br/> Consume <Text as='mark'>{results.keep}</Text> calories per day <br/>
+        To <b>gain</b> weight: <br/> Consume <Text as='mark'>{results.gain}</Text> calories per day <br/>
+      </ModalBody>
+      <ModalFooter>
+        <Button colorScheme='blue' mr={3} onClick={onClose}>
+          Close
+        </Button>
+      </ModalFooter>
+    </ModalContent>
+  </Modal>
+  )
+}
