95b1fc7e5e7ec863775d05fc9a8bee91228aeae9
felipegsantos15 <felipegoncalvesdsantos@gmail.com>
2023-10-14 01:50:47 -0300

    refactor: add less complexity to server

--- backend/src/adapters/express/server.ts
+++ backend/src/adapters/express/server.ts
@@ -1,42 +1,22 @@
 import Weight from '../../domain/use-cases/weight'
-import Water from '../../domain/use-cases/water-use'
+import Water from '../../domain/use-cases/water'
 import Creatine from '../../domain/use-cases/creatine'
 import express, { Request, Response } from 'express'
 import cors from 'cors'
 
 const app = express()
+const water = new Water()
+const weight = new Weight()
+const creatine = new Creatine()
 
 app.use(express.json())
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
-app.post('/weight', (req: Request, res: Response) => {
-  const personData = req.body
-  const weight = new Weight(personData)
-  const result = {
-    'keep': weight.toKeep(),
-    'gain': weight.toGain(),
-    'lose': weight.toLose()
-  }
-  res.status(200).send(result)
-})
-
-app.post('/water', (req: Request, res: Response) => {
-  const { weight, factor } = req.body
-  const water = new Water(weight, factor)
-  const result = {
-    'waterConsumption': water.calculateDailyQuantity()
-  }
-  res.status(200).send(result)
-})
-
-app.post('/creatine', (req: Request, res: Response) => {
-  const { weight } = req.body
-  const creatine = new Creatine(weight)
-  const result = creatine.calculate();
-  res.status(200).send(result)
-})
+app.post('/weight', weight.main)
+app.post('/water', water.main)
+app.post('/creatine', creatine.main)
 
 app.listen(process.env.PORT, () => {
   console.log(`listen http server on port: ${process.env.PORT}`)
